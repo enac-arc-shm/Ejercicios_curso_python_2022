1b98635161c7c44241c8786e5c61430867710646
enac-arc-shm <21300018@uttt.edu.mx>
2022-10-27 19:22:26 -0500

    operaciones

--- Ejercicio1_operaciones.py
+++ Ejercicio1_operaciones.py
@@ -1,1 +1,21 @@
-print("hola mundo")
+#Operaciones numericas
+from cmath import sin #Libreria necesaria para operaciones complejas como seno, raiz, etc
+
+#Solicitar numeros y hacer la conversionde str a int 
+numero1 = int(input("Ingresa un número: "))
+numero2 = int(input("Ingresa otro número: "))
+
+#operaciones basicas de python
+suma = numero1 + numero2
+multiplicacion = numero1 * numero2
+reciduo = numero1 % numero2
+division_entera = numero1 // numero2
+potencia = numero1 ** numero2
+
+#Operacion seno de la libreria math 
+seno = sin(numero1)
+
+#impresion de operaciones
+print("suma = ", suma, "multiplicacion = ",multiplicacion)
+print("potenccia = ", potencia, "reciduo = ",reciduo)
+print("division = ", division_entera)
